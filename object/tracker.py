9d50c9db8f3ddf9811c869008b7e425b66e1bf03
dhruva-star88 <dhruvatej6565@gmail.com>
2024-11-06 15:59:09 +0530

    stop sign real time tracker

--- object/tracker.py
+++ object/tracker.py
@@ -1,32 +1,37 @@
 import cv2
 
-# Initialize tracker
-tracker = cv2.legacy.TrackerKCF_create()
-init_box = None  
-# Assuming init_box is the bounding box of detected object
-video = cv2.VideoCapture(0)
+stopsign_classifier = cv2.CascadeClassifier(
+ "D:\Opencv\object\stop_data.xml"
+)
+# Access the webcam
+video_capture = cv2.VideoCapture(0)
 
-# Read first frame
-ok, frame = video.read()
-if ok:
-    init_box = cv2.selectROI(frame, False)  # Let user select ROI
-    tracker.init(frame, init_box)
+# Function identify faces in Cam and draw the bounding box around it
+def detect_bounding_box(vid):
+    gray_image = cv2.cvtColor(vid, cv2.COLOR_BGR2GRAY)
+    sign = stopsign_classifier.detectMultiScale(gray_image, 1.1, 5, minSize=(40, 40))
+    for (x, y, w, h) in sign:
+        cv2.rectangle(vid, (x, y), (x + w, y + h), (0, 255, 0), 4)
+    return sign
 
+# Creating a Loop for Real-Time Face Detection
 while True:
-    ok, frame = video.read()
-    if not ok:
-        break
-    
-    ok, bbox = tracker.update(frame)
-    if ok:
-        (x, y, w, h) = [int(v) for v in bbox]
-        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2, 1)
-    else:
-        cv2.putText(frame, "Tracking failure", (100, 80), cv2.FONT_HERSHEY_SIMPLEX, 0.75,(0,0,255),2)
+    # read frames from the video
+    result, video_frame = video_capture.read()  
+    if result is False:
+        # terminate the loop if the frame is not read successfully
+        break  
+# apply the function we created to the video frame
+    sign = detect_bounding_box(
+        video_frame
+    )  
+# display the processed frame in a window named "My Face Detection Project"
+    cv2.imshow(
+        "My Face Detection Project", video_frame
+    )  
 
-    cv2.imshow("Tracking", frame)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    if cv2.waitKey(1) & 0xFF == ord("q"):
         break
 
-video.release()
+video_capture.release()
 cv2.destroyAllWindows()
